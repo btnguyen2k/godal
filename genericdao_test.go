fb963860de14a5778d0066851dd4106dc598718a
Thanh Nguyen <thanhn@microsoft.com>
2020-11-26 22:05:18 +1100

    add more tests

---
+++ genericdao_test.go
@@ -0,0 +1,41 @@
+package godal
+
+import (
+	"testing"
+)
+
+func TestNewAbstractGenericDao(t *testing.T) {
+	name := "TestNewAbstractGenericDao"
+	dao := NewAbstractGenericDao(nil)
+	if dao == nil {
+		t.Fatalf("%s failed: nil", name)
+	}
+}
+
+type mockRowMapper struct {
+}
+
+func (m *mockRowMapper) ToRow(storageId string, bo IGenericBo) (interface{}, error) {
+	panic("implement me")
+}
+
+func (m *mockRowMapper) ToBo(storageId string, row interface{}) (IGenericBo, error) {
+	panic("implement me")
+}
+
+func (m *mockRowMapper) ColumnsList(storageId string) []string {
+	panic("implement me")
+}
+
+func TestAbstractGenericDao_GetSetRowMapper(t *testing.T) {
+	name := "TestNewAbstractGenericDao"
+	dao := NewAbstractGenericDao(nil)
+	rowMapper := &mockRowMapper{}
+	dao2 := dao.SetRowMapper(rowMapper)
+	if dao2 == nil {
+		t.Fatalf("%s failed: nil", name)
+	}
+	if v := dao.GetRowMapper(); v != rowMapper {
+		t.Fatalf("%s failed: expected %p but received %p", name, rowMapper, v)
+	}
+}
